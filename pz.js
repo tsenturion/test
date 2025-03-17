922e7e952e7bea19ba0784edf4b276083524d8e0
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-17 11:37:58 +0500

    pz

---
+++ pz.js
@@ -0,0 +1,11 @@
+//npm install --save-dev jest
+//package
+//first.test.js
+//npm run test
+
+/*
+1. любую мат функцию
+2. describe it - функцию сложения
+3. мокинг - создайте функцию, вызывающую другую функцию. Например add из math.js
+4. тест с мокингом для новой функции doAdd
+*/
