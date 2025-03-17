47a18e0bf5fe5bf5fb60d1375d14b7b363fcfafd
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-17 13:47:52 +0500

    node.js

--- fromCourse.js
+++ fromCourse.js
@@ -17,3 +17,23 @@ if (capitalaize("") !== '') {
 // пограничный случай
 // ошибочный сценарий
 
+import { strict as assert } from 'node:assert';
+import capitalize from '../src/capitalize.js';
+
+assert.strictEqual(capitalize('hello'), 'Hello');
+
+/*
+AssertionError [ERR_ASSERTION]: 'hello' == 'Hello'
+  actual: 'hello',
+  expected: 'Hello'
+*/
+assert.strictEqual(1, '1'); // Ошбика
+
+assert.deepStrictEqual({ key: "value" }, {key: "value" }); 
+assert.deepStrictEqual({ key: "value" }, {key: "another value" }); 
+
+//с not
+assert.notstrictEqual(1, '1'); // Ошбика
+
+assert.notdeepStrictEqual({ key: "value" }, {key: "value" }); 
+assert.notdeepStrictEqual({ key: "value" }, {key: "another value" }); 
