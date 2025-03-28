4ad84d28c08b11aa515d56c931b7455206a2a262
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-03-28 09:42:07 +0500

    closure

--- python/theory.py
+++ python/theory.py
@@ -1,25 +1,12 @@
 """
 Линтинг.
-Бэкенд на Python. 
-Протокол HTTP. 
 Микрофреймворки. 
 Шаблонизация. 
-СУБД. 
 ORM. 
-MVC. 
-Fullstack-фреймворки. 
 Тесты. 
 
 Хеш-таблицы. 
-Позиционные аргументы. 
-Именованные аргументы.
-Больше об именованных аргументах. 
-Операторы упаковки и распаковки. 
-Функции высшего порядка. 
-Знакомство с map, filter и reduce. 
-Чистые функции. 
 Замыкания. 
-Анонимные функции. 
 Декораторы. 
 Больше о декораторах. 
 Рекурсия. 
@@ -41,8 +28,6 @@ Fullstack-фреймворки.
 Уровневое проектирование. 
 Инварианты.
 Определения. 
-Виртуальная файловая система. 
-Манипуляции с виртуальной файловой системой. 
 Обход дерева. 
 Агрегация.
 Аккумулятор
@@ -64,16 +49,11 @@ Fluent Interface.
 Пишем код правильно
 Представление символов и метасимвол.
 Символьные классы. 
-Позиция внутри строки. 
-Альтернатива. 
 Квантификация. 
 Жадность. 
 Группировка и обратная связь.
 Модификаторы. 
 Просмотр вперед и назад. 
-Поиск по условию. Флаги
-Архитектура Веба. 
-Веб-сервер. 
 Встроенный веб-сервер. 
 Flask. 
 Обработчики запросов.
@@ -1395,3 +1375,298 @@ print(list(map(lambda x : x ** 2, numbers)))
 
 # Отфильтруйте чётные числа, а затем возведите их в квадрат. map filter
 print(list(map(lambda x : x ** 2, filter(lambda x : x % 2 == 0, numbers))))
+
+#Замыкания. 
+#curry
+#хеш таблицы
+# nonlocal для изменения переменных во вложенных функциях
+# global для работы с глобальными переменными внутри функций
+#legb
+some_const = 0
+
+def some():
+    count = 0
+    print(count)
+
+some()
+
+x = 10
+
+def change_x():
+    global x
+    x = 20
+    print(x)
+change_x()
+print(x)
+
+#nonlocal
+
+def outer():
+    x = 10
+    def inner():
+        nonlocal x
+        x = 20
+        print(x)
+    inner()
+    print(x)
+
+outer()
+
+
+
+"""
+для замыканий
+"""
+#замыкания
+x = 5
+def outer():
+    x = 10
+
+    def inner():
+        print(x)
+
+    return inner
+
+func = outer()
+func()
+
+def counter():
+    count = 0
+    def increment():
+        nonlocal count
+        count += 1
+        return count
+    return increment
+
+counter1 = counter()
+print(counter1())
+print(counter1())
+print(counter1())
+
+counter2 = counter()
+
+"""
+сохранени состояния
+сокрытие данных
+передача параметров без глобальных переменных
+фабрики функций
+"""
+
+"""
+память
+менее читаемый
+nonlocal
+"""
+
+
+"""
+изменение глобальных внутри функции
+сохранение между вызовами функций
+"""
+
+"""
+def outer_function(x):
+    def inner_function(y):
+        return x + y
+    return inner_function
+
+closure = outer_function(10)
+
+result = closure(5)
+print(result)
+
+def counter():
+    count = 0
+    def increment():
+        nonlocal count
+        count += 1
+        return count
+    return increment
+counter1 = counter()
+print(counter1())
+print(counter1())
+print(counter1())
+
+# фабрика функций 
+def multiplier(n):
+    def multiply(x):
+        return  n * x
+    return multiply
+
+double = multiplier(2)
+triple = multiplier(3)
+
+print(double(5))
+print(triple(5))
+"""
+
+"""
+def возвращает словарь счетчика с методами
++1
+-1
+текущее
+сброс к начальному
+история изменений
+откат к предыдущему
+"""
+"""
+def smart_counter(initial_value=0):
+    value = initial_value
+    history_list = [initial_value]
+
+    def increment():
+        nonlocal value
+        pass
+
+    def decrement():
+        nonlocal value
+        value -= 1
+        history_list.append(value)
+
+    def get_value():
+        return value
+
+    def history():
+        return history_list
+
+    def undo():
+        nonlocal value
+        if len(history_list) > 1:
+            history_list.pop()
+            value = history_list[-1]
+
+    return {
+        "increment": increment,
+        "decrement": decrement,
+        "get_value": get_value,
+        #reset
+        "history": history,
+        "undo": undo,
+    }
+
+counter = smart_counter(5)
+counter['history']()
+counter['undo']()
+counter['decrement']()
+"""
+
+#каррирование - разбирваем функцию с n аргументов на n вложенных функций
+"""
+упрощение работы с func высшего порядка
+частичное применение функции
+для функционального программирования, улучшает читаемость, логичность
+"""
+"""def add(a, b):
+    return a + b
+
+def add_curried(a):
+    def inner(b):
+        return a + b
+    return inner
+
+result = add_curried(2)(3)
+print(result)
+
+add_2 = add_curried(2)
+print(add_2(3))
+
+def add(a):
+    def inner1(b):
+        def inner2(c):
+            return a + b + c
+        return inner2
+    return inner1
+
+
+result = add(1)(2)(3)
+
+from functools import partial
+add_two = partial(add, 2)
+result = add_two(3)
+print(result)
+
+def curry(func):
+    def curried(*args):
+        if len(args) >= func.__code__.co_argcount:
+            return func(*args)
+        return lambda *more_args: curried(*(args + more_args))
+    return curried
+
+@curry
+def add(a, b, c):
+    return a + b + c
+
+result = add(1)(2)(3)
+print(result)"""
+
+"""
+add
+subtract
+multiply
+divide"""
+from functools import partial
+"""def operation(a):
+    def inner(b):
+        def oper(op):
+            try:
+                if op == 'add':
+                    return a + b
+                elif op == "subtract":
+                    return a - b
+                elif op == "divide":
+                    if b == 0:
+                        raise ZeroDivisionError('123')
+                    return a / b
+                elif op == 'multiply':
+                    return a * b
+                else:
+                    raise ValueError('1234')
+            except ValueError as e:
+                return str(e)
+            except ZeroDivisionError as e:
+                return str(e)
+            except Exception as ex:
+                return ex
+        return oper
+    return inner
+"""
+"""result = operation(10)(5)('add')
+print(result)
+result = operation(10)(0)('divide')
+print(result)
+print(result is None)
+result = operation(10)(5)('adde')
+print(result)
+print(result is None)"""
+
+"""def operation(a, b, op):
+    try:
+        if op == 'add':
+            return a + b
+        elif op == "subtract":
+            return a - b
+        elif op == "divide":
+            if b == 0:
+                raise ZeroDivisionError('123')
+            return a / b
+        elif op == 'multiply':
+            return a * b
+        else:
+            raise ValueError('1234')
+    except ValueError as e:
+        return str(e)
+    except ZeroDivisionError as e:
+        return str(e)
+    except Exception as ex:
+        return ex
+
+add = partial(operation, op='add')
+add_10 = partial(partial(operation, a=10), op='add')
+divide = partial(operation, op='divide')
+divide2 = partial(operation, op='d3ivide')
+print(add(10, 2))
+print(add_10(b=2))
+print(divide(10, 2))
+print(divide(10, 0))
+print(divide2(10, 2))
+"""
